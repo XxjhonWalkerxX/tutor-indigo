5636bcb00587e3961b9179e5d0215384f7b3b327
jadrian_s <jadrian.s@gmail.com>
2025-01-16 13:45:10 -0600

    Setup proyecto

--- mexicoxmx/plugin.py
+++ mexicoxmx/plugin.py
@@ -8,6 +8,7 @@ from tutor import hooks
 from tutor.__about__ import __version_suffix__
 
 from .__about__ import __version__
+from tutormfe.hooks import PLUGIN_SLOTS
 
 # Handle version suffix in nightly mode, just like tutor core
 if __version_suffix__:
@@ -37,7 +38,6 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
     "unique": {},
     "overrides": {},
 }
-
 # Theme templates
 hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
     str(importlib_resources.files("mexicoxmx") / "templates")
@@ -46,6 +46,7 @@ hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
 hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
         ("mexicoxmx", "build/openedx/themes"),
+        ("mexicoxmx/env.config.jsx", "plugins/mfe/build/mfe"),
     ],
 )
 
@@ -84,9 +85,9 @@ def _override_openedx_docker_image(
         elif k == "MFE_DOCKER_IMAGE":
             mfe_image = v
     if openedx_image:
-        items.append(("DOCKER_IMAGE_OPENEDX", f"{openedx_image}"))
+        items.append(("DOCKER_IMAGE_OPENEDX", f"{openedx_image}-mexicoxmx"))
     if mfe_image:
-        items.append(("MFE_DOCKER_IMAGE", f"{mfe_image}"))
+        items.append(("MFE_DOCKER_IMAGE", f"{mfe_image}-mexicoxmx"))
     return items
 
 
@@ -100,48 +101,118 @@ hooks.Filters.CONFIG_UNIQUE.add_items(
 hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
 
+#  MFEs that are styled using Indigo
+indigo_styled_mfes = [
+    "learning",
+    "learner-dashboard",
+    "profile",
+    "account",
+    "discussions",
+]
+
 hooks.Filters.ENV_PATCHES.add_items(
     [
         (
-            "mfe-dockerfile-post-npm-install-learning",
+            f"mfe-dockerfile-post-npm-install-{mfe}",
             """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
+           
+RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
+RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
+RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
+
 """,
-            # remove indigo-header and indigo-footer due to incompatible version deps of MFEs
+        )
+        for mfe in indigo_styled_mfes
+    ]
+)
+
+
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-dockerfile-post-npm-install-authn",
+        "RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'",
+    )
+)
+
+# Include js file in lms main.html, main_django.html, and certificate.html
+
+hooks.Filters.ENV_PATCHES.add_items(
+    [
+        # for production
+        (
+            "openedx-common-assets-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['mexicoxmx/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+""",
+        ),
+        # for development
+        (
+            "openedx-lms-development-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['indigo/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+
+MFE_CONFIG['INDIGO_ENABLE_DARK_TOGGLE'] = {{ INDIGO_ENABLE_DARK_TOGGLE }}
+""",
         ),
         (
-            "mfe-dockerfile-post-npm-install-authn",
+            "openedx-lms-production-settings",
             """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-""",
-        ),
-        # Tutor-Indigo v2.1 targets the styling updates in discussions and learner-dashboard MFE
-        # brand-openedx is related to styling updates while others are for header and footer updates
-        (
-            "mfe-dockerfile-post-npm-install-discussions",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-""",
-            # remove indigo-header and indigo-footer due to incompatible version deps of MFEs
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-learner-dashboard",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-""",
-            # remove indigo-footer due to incompatible version deps of MFEs
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-profile",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-account",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
+MFE_CONFIG['INDIGO_ENABLE_DARK_TOGGLE'] = {{ INDIGO_ENABLE_DARK_TOGGLE }}
 """,
         ),
     ]
 )
+
+
+# Apply patches from tutor-indigo
+for path in glob(
+    os.path.join(
+        str(importlib_resources.files("mexicoxmx") / "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+for mfe in indigo_styled_mfes:
+    PLUGIN_SLOTS.add_item(
+        (
+            mfe,
+            "footer_slot",
+            """ 
+            {
+                op: PLUGIN_OPERATIONS.Hide,
+                widgetId: 'default_contents',
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'default_contents',
+                    type: DIRECT_PLUGIN,
+                    priority: 1,
+                    RenderWidget: <IndigoFooter />,
+                },
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'read_theme_cookie',
+                    type: DIRECT_PLUGIN,
+                    priority: 2,
+                    RenderWidget: AddDarkTheme,
+                },
+            },
+  """,
+        ),
+    )
