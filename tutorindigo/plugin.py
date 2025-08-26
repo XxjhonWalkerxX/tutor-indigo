1a373a44847ac07880bcd7e4ddf3ec7f86a804b7
jadrians <jadrian.s@gmail.com>
2025-08-26 14:21:55 -0600

    merging old with new

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -21,24 +21,24 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        "WELCOME_MESSAGE": "The place for all your online learning",
-        "PRIMARY_COLOR": "#15376D",  # Indigo
-        "ENABLE_DARK_TOGGLE": True,
+        "WELCOME_MESSAGE": "Plataforma de entrenamiento digital educativo para instituciones públicas y de interés educativo",
+        "PRIMARY_COLOR": "#82c5be",  # Indigo
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
         "FOOTER_NAV_LINKS": [
-            {"title": "About Us", "url": "/about"},
-            {"title": "Blog", "url": "/blog"},
-            {"title": "Donate", "url": "/donate"},
-            {"title": "Terms of Service", "url": "/tos"},
-            {"title": "Privacy Policy", "url": "/privacy"},
-            {"title": "Help", "url": "/help"},
-            {"title": "Contact Us", "url": "/contact"},
+            {"title": "Sobre el proyecto", "url": "/about"},
+            {"title": "Boletín informativo", "url": "https://mail.mexicox.gob.mx/lists/?p=subscribe"},
+            {"title": "Código de honor", "url": "/honor"},
+            {"title": "Aviso de privacidad", "url": "/tos"},
+            {"title": "Preguntas frecuentes", "url": "/help"},
+            {"title": "Contacto", "url": "/contact"},
+            {"title": "Soporte", "url": "https://soporte.mexicox.gob.mx"},
         ],
     },
     "unique": {},
     "overrides": {},
+
 }
 
 # Theme templates
